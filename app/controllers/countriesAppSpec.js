2282bd8029fa913a5944f50e020be09594111b87
Rakesh Patel <emailme@rakeshrpatel.com>
2016-05-31 20:24:38 -0400

    unit tests

---
+++ app/controllers/countriesAppSpec.js
@@ -0,0 +1,27 @@
+describe('getCountryData', function(){
+	beforeEach(module('countriesApp'));
+
+	it('should look up country details for listed nations', 
+		inject(function(getCountryData, $rootScope, $httpBackend){
+			$httpBackend.expect('GET', 'http://api.geonames.org/countryInfoJSON?').respond(200);
+			var status = false;
+			getCountryData('success').then(function(){
+				status = true;
+			});
+
+			$rootScope.$digest();
+			$httpBackend.flush();
+			expect(status).toBe(true);
+			$httpBackend.verifyNoOutStandingRequest();
+		}));
+
+
+})
+
+// describe('countriesCtrl', function(){
+// 	beforeEach(module('countriesApp'));
+
+// 	var $controller; 
+
+// 	it('should store data into the rootScope value')
+// 	})
